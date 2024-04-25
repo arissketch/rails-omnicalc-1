2cdc60e385d5fcf20d2159e8cb729f9261e727a6
arissketch <60331491+arissketch@users.noreply.github.com>
2024-04-25 01:42:01 +0000

    final

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,45 @@ class MainController < ApplicationController
     render({:template=>"calculate/square"})
   end
   def squareresults
+    @mynumber=params.fetch("square_number").to_f
+    @square=@mynumber**2
     render({:template =>"calculate/squareresults"})
   end
 
+  def squareroot
+    render({:template=>"calculate/squareroot"})
+  end
+
+  def squarerootresults
+    @mynumber=params.fetch("sqrt_number").to_f
+    @sqrt=@mynumber**(1/2.0)
+    render({:template=>"calculate/squarerootresults"})
+  end
+
+
+  def payment
+    render({:template=>"calculate/payment"})
+  end
+
+  def paymentresults
+    @apr=params.fetch("apr").to_f
+    @principal=params.fetch("principal").to_f
+    @year=params.fetch("year").to_f
+
+    @numerator = (@apr/100/12)*@principal
+    @denominator = (1-(1+@apr/100/12)**(-1*@year*12))
+    @payment =@numerator/@denominator
+    render({:template=>"calculate/paymentresults"})
+  end
+
+  def random
+    render({:template=>"calculate/random"})
+  end
+
+  def randomresults
+    @min=params.fetch("min").to_f
+    @max=params.fetch("max").to_f
+    @random=rand(@min...@max).to_f
+    render({:template=>"calculate/randomresults"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,14 @@ Rails.application.routes.draw do
   get("/square/new", {:controller => "main", :action => "square"})
   get("/square/results", {:controller => "main", :action => "squareresults"})
 
+  get("/square_root/new", {:controller => "main", :action => "squareroot"})
+  get("/square_root/results", {:controller => "main", :action => "squarerootresults"})
 
+  get("/payment/new", {:controller => "main", :action => "payment"})
+  get("/payment/results", {:controller => "main", :action => "paymentresults"})
+
+  get("/random/new", {:controller => "main", :action => "random"})
+  get("/random/results", {:controller => "main", :action => "randomresults"})
 
   
 end
